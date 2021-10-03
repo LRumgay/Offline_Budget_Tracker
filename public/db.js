04105c470b65ec041b032371053a572086b9d759
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-02 22:16:49 -0500

    ...

--- public/db.js
+++ public/db.js
@@ -3,5 +3,5 @@ const request = window.indexedDB.open("budgetDB", 1);
 
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore("pending", {autoincrement: true});
+    db.createObjectStore("budgetDB", {autoincrement: true});
 };
